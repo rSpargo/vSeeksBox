19c49b2521f8fc6f38b1e0b315f3ef89105a01a2
Ryan Spargo <ryanspargo@gmail.com>
2017-10-12 10:12:02 -0600

    Obligatory commit.

--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -4,7 +4,7 @@ function newVSeeks() {
     var content = "<form class='vseeks-form' name='vSeekForm'>";
     content += "Task:<br />";
     content += "<input type='text' name='task'><br />";
-    content += "<input type='submit' value= 'Submit' onClick='createVSeeks(vSeekForm.task.value)'>";
+    content += "<input type='submit' value= 'Submit'";
     content += "</form>";
     body.innerHTML = content;
 }
