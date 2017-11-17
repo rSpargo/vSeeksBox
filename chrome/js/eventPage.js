7f147cd32e10aed7f65831356b5b5172882bc1d7
Ryan Spargo <ryanspargo@gmail.com>
2017-11-17 11:19:51 -0700

    Major progress push

--- chrome/js/eventPage.js
+++ chrome/js/eventPage.js
@@ -1,17 +1,78 @@
-//onload database to local storage
-var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'https://vseeks-box.herokuapp.com/getData/newuser');
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var data = JSON.parse(this.responseText);
-        chrome.storage.sync.set({'userData': data}, function() {
-            console.log('Data saved to local storage: ', data);
-        });
+chrome.identity.getAuthToken({interactive:true}, function(token) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=' + token);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("Received data: ", JSON.parse(this.responseText));
+            var authData = JSON.parse(this.responseText);
+
+            var db = new XMLHttpRequest();
+            db.open('GET', 'https://vseeks-box.herokuapp.com/getData/' + authData.id);
+            db.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    var chromeData = JSON.parse(this.responseText);
+                    chrome.storage.sync.set({'userData': chromeData}, function() {
+                        console.log('Data saved to local storage: ', chromeData);
+                    });
+                }
+            }
+            db.send();
+        }
     }
-}
-xhttp.send();
+    xhttp.send();
+});
 
 //listen for changes in local storage
 chrome.storage.onChanged.addListener(function(changes) {
-    
-})
+    var totalVSeeks = changes.userData.newValue.vSeeks.length;
+    console.log(changes);
+    chrome.browserAction.setBadgeText({"text": totalVSeeks.toString()});
+});
+
+//listen for alarm
+chrome.alarms.onAlarm.addListener(function(alarm) {
+    console.log("Alarm triggered!", alarm);
+    var notif = {
+        type: 'basic',
+        title: "Look at me!",
+        message: 'The time you set is up! Are you finished?',
+        iconUrl: '../assets/icon.png',
+        buttons: [
+            {title: "Nope! I need more time."},
+            {title: "Yep! You're free to go."}
+        ]
+    }
+    var tone = new Audio('../assets/tada.wav');
+    chrome.notifications.create(alarm.name, notif, function(){ });
+    tone.play();
+});
+
+//listen for notification button press
+chrome.notifications.onButtonClicked.addListener(function(id, index) {
+    if (index === 0) {
+        console.log("Option button: NO, clicked");
+        //keep active
+        //keep notifying
+    } 
+    else {
+        //disarm alarm
+        chrome.alarms.clear(id);
+        chrome.notifications.clear(id);
+        //delete from storage
+        chrome.storage.sync.get('userData', function(items) {
+            var newData = items.userData;
+            var vSeek = newData.vSeeks.findIndex(i => i.id === id);
+            newData.vSeeks.splice(vSeek, 1);
+            console.log(newData.vSeeks);
+            chrome.alarms.getAll(function(alarms) {
+                console.log("Currently active alarms: ", alarms);
+            });
+            chrome.notifications.getAll(function(notifications) {
+                console.log("Notifications: ", notifications);
+            });
+            chrome.storage.sync.set({'userData': newData});       
+        });
+        //reaccess websites
+        
+    }
+});
--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -8,22 +8,58 @@ app.config(function($routeProvider) {
         templateUrl: '../html/new-vseeks.html',
         controller: 'vSeeksController'
     })
+    .when('/detail/:id', {
+        templateUrl: '../html/vseeks-detail.html',
+        controller: 'detailController'
+    })
 });
-app.controller('mainController', function($scope, $http) {
-    $http.get("https://vseeks-box.herokuapp.com/getVSeeks/newuser")
-    .then(function(response) { $scope.vseeks = response.data });
+app.controller('mainController', function($scope, $window) {
+    /* $http.get("https://vseeks-box.herokuapp.com/getVSeeks/newuser")
+    .then(function(response) { $scope.vseeks = response.data }); */
+    chrome.storage.sync.get('userData', function(items){
+        $scope.vseeks = items.userData.vSeeks;
+    });
+    $scope.viewDetail = function(vseek) {
+        console.log("vSeek: ", vseek);
+        var index = $scope.vseeks.indexOf(vseek);
+        console.log("index: ", index);
+        $window.location.href = '#!/detail/' + index;
+    }
 });
-app.controller('vSeeksController', function($scope, $http) {
+app.controller('vSeeksController', function($scope, $window, $route, $http) {
     $scope.createVSeeks = function() {
-        var data = {
-            task: $scope.task,
-            timer: {
-                hours: $scope.hours,
-                minutes: $scope.minutes,
-                seconds: $scope.seconds
-            }
-        };
-        $http.post('https://vseeks-box.herokuapp.com/saveVSeeks/newuser', data)
+        $http.get('https://vseeks-box.herokuapp.com/genID')
+        .then(function(response) {
+             $scope.id = response.data;
+
+             var data = {
+                id: $scope.id,
+                task: $scope.task,
+                timer: {
+                    hours: $scope.hours,
+                    minutes: $scope.minutes,
+                    seconds: $scope.seconds
+                }
+            };
+            console.log("new vSeek data", data);
+            chrome.storage.sync.get('userData', function(items) {
+                var newData = items.userData;
+                console.log(newData);
+                newData.vSeeks.push(data);
+                chrome.storage.sync.set({'userData': newData});
+    
+                var alarmTime = {
+                    when: Date.now() + convertToMilli(data.timer.hours, data.timer.minutes, data.timer.seconds)
+                }
+                chrome.alarms.create(data.id, alarmTime);        
+            });
+        });
+
+
+    //$window.location.reload();
+    //$window.location.href = '#!/';
+        
+        /* $http.post('https://vseeks-box.herokuapp.com/saveVSeeks/newuser', data)
         .then(function (response) {
             var notif = {
                 type: 'basic',
@@ -32,11 +68,31 @@ app.controller('vSeeksController', function($scope, $http) {
                 iconUrl: '../assets/icon.png'
             }
 
+
+
             var tone = new Audio('../assets/tada.wav');
 
             chrome.notifications.create('newVSeeks', notif, function() { });
             tone.play();
             window.location.href = '#/!';
-        });
+        }); */
     }
+    function convertToMilli (hrs, mins, secs) {
+        console.log("conversion method hit")
+        var result = 0;
+        result += (hrs * 3600000);
+        result += (mins * 60000);
+        result += (secs * 1000);
+        console.log("result: ", result)
+        return result;
+    }
+});
+app.controller('detailController', function($scope, $routeParams){
+    var data = "";
+    console.log("Controller called. Route params: ", $routeParams);
+    chrome.storage.sync.get('userData', function(items){
+        $scope.currentVSeek = items.userData.vSeeks[$routeParams.id];
+        console.log($scope.currentVSeek);
+    });
+    console.log($scope.currentVSeek);
 });
